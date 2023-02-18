8bdf7c652cc6684e237f64ccd0890539b8a4d290
erncat2 <125676656+erncat2@users.noreply.github.com>
2023-02-18 16:38:27 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -90,7 +90,7 @@ export default function Home() {
       <main>
         <section className={styles.Hero}>
           <ErinAndBenoitLogo />
-          <h2>06/03/2023</h2>
+          <h1>06/03/2023</h1>
           <p className={styles.JoinUs}>
             Please join us out in the boonies
             to celebrate our love
