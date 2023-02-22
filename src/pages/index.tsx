345eb6532187cdd0fd4199c08e2492c403c31699
erncat2 <125676656+erncat2@users.noreply.github.com>
2023-02-22 18:43:40 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -154,8 +154,7 @@ export default function Home() {
             <p className="large">Holiday Inn Gatineau</p>
             <p>7 Bd du Casino, Gatineau, QC</p>
             <p>
-              To book using group discount, call 8195252255 or <a href="https://www.holidayinn.com/redirect?path=hd&brandCode=HI&localeCode=en&regionCode=1&hotelCode=YOWGN
-                &_PMID=99801505&GPC=NOW&cn=no&viewfullsite=true">Book Online.</a>
+              To book using group discount, call 8195252255 or <a href="https://www.ihg.com/holidayinn/hotels/us/en/gatineau/yowgn/hoteldetail?fromRedirect=true&qSrt=sBR&qSlH=YOWGN&setPMCookies=true&qSHBrC=HI&qDest=7+boulevard+du+Casino%2C+Gatineau%2C+CA&srb_u=1">Book Online.</a>
               Group discount code “ERC”. Group discount extended 3 days
               post/prior June 3.
             </p>
