d2be6a4509606d1f586b86aa1e045795bc943b43
erncat2 <125676656+erncat2@users.noreply.github.com>
2023-02-22 20:42:13 -0500

    Update morningafter.tsx

--- src/pages/morningafter.tsx
+++ src/pages/morningafter.tsx
@@ -18,9 +18,9 @@ export default function TheMorningAfter() {
 
     <h1>9-11am</h1>
 
-    <p className="large">
+    <h1>
       Holiday Inn Gatineau
-    </p>
+    </h1>
     <p>
       7 Bd du Casino, Gatineau, QC
     </p>
